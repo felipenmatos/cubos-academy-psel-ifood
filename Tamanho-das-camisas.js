4acb7d17f3f93ef8f16eaadee3118e6a20e88b75
Felipe Nonato <82072640+Lipe-27@users.noreply.github.com>
2021-08-11 12:02:51 -0300

    camisas

---
+++ Tamanho-das-camisas.js
@@ -0,0 +1,15 @@
+function solucao(tamanhoBr) {
+    let roupa = tamanhoBr; 
+    if (roupa === "PP") {
+        console.log("XS")
+    } else if ( roupa === "P") {
+        console.log("S")
+    } else if (roupa === "M") {
+        console.log("M")
+    } else if (roupa === "G") {
+        console.log("L")
+    } else {
+        console.log("XL")
+    }
+  }
+  
